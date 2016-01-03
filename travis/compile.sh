6d8df89c270691ff89463a9bfcbdb75ce680a66a
NauxLiu <nauxliu@gmail.com>
2016-01-03 18:23:01 +0800

    update travis

--- travis/compile.sh
+++ travis/compile.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-apt-get install doxygen
 
-git clone https://github.com/BYVoid/OpenCC.git --depth 1
-
-cd OpenCC
-make && make test && make install
-
-cd ..
 phpize && ./configure && make clean && make
---
+++ travis/install_opencc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git clone https://github.com/BYVoid/OpenCC.git --depth 1
+
+cd OpenCC
+make && make test && make install
