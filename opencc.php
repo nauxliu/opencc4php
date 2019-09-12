b2e029e78041060d82dbe94c30e3b8b00ae8a372
Xiangxuan Liu <xiangxuan.liu@rightcapital.com>
2019-09-12 21:36:51 +0800

    Clean up opencc.php

--- opencc.php
+++ opencc.php
@@ -10,10 +10,3 @@ echo "Functions available in the test extension:$br\n";
 foreach($functions as $func) {
     echo $func."$br\n";
 }
-echo "$br\n";
-
-$od = opencc_open('s2twp.json');
-echo opencc_convert("我的鼠标哪儿去了？", $od);
-echo opencc_error()."\n";
-var_dump(opencc_close($od));
-?>
